7ac41bc163b41804bc0620e0377a3f4a67ab1215
Mike Adams <mikethebos@icloud.com>
2023-02-26 14:49:42 -0700

    add P(X|Y) estimate

--- src/utils.py
+++ src/utils.py
@@ -45,6 +45,21 @@ def get_xi_in_yk(mat: np.ndarray, vocab_size: int):
         arr[i, :] = cnts
     return arr
 
+def get_P_of_xi_given_yi(mat: np.ndarray, vocab_size: int, a: float):
+    """
+    Estimate P(X|Y) using a MAP estimate.
+    :param mat: np.ndarray, rows are entries. labels expected in last column
+    :param vocab_size: int, number of words in vocabulary
+    :param a: float, alpha value used to smooth estimate
+    :return arr, np.ndarray of shape (label_sive x vocab_size) where
+        arr[j][i] indicates P(Xi | Yj)
+    """
+    xi_in_yk_counts = get_xi_in_yk(mat, vocab_size)  # (label_size x vocab_size)
+    word_count_in_yk = np.sum(xi_in_yk_counts, axis=1)  # (label_size, )
+    denom = word_count_in_yk + ((a - 1) * vocab_size)  # (label_size, )
+    numer = xi_in_yk_counts + (a - 1)  # (label_size x vocab_size)
+    arr = numer / denom.reshape((-1, 1))
+    return arr
 
 if __name__ == "__main__":
     mat1 = sparse.load_npz("../data/sparse_training.npz").toarray()
@@ -56,3 +71,6 @@ if __name__ == "__main__":
     # print(mat1)
     x = get_xi_in_yk(mat1, 61188)
     print(x)
+    estxi_given_yk = get_P_of_xi_given_yi(mat1, 61188, 1 / 61188)
+    print(estxi_given_yk)
+    print(estxi_given_yk.shape)
