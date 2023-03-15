9514ef0176a58f1a0ca33bd2fe5c27ba1c0a8a1f
Jack-42 <johnjack0987@gmail.com>
2023-03-15 15:43:20 -0600

    Add plot for LR gridsearch

--- src/plots.py
+++ src/plots.py
@@ -12,14 +12,14 @@ Description:
 """
 
 
-def plot_nb_gridsearch(results_csv: str, save_pth: str = None):
+def plot_nb_gridsearch(results_csv_path: str, save_pth: str = None):
     """
     Plot the results of the param search for Naive-Bayes
-    :param results_csv: str, path to results.csv file
+    :param results_csv_path: str, path to results.csv file
     :param save_pth: str (optional), path to save figure to
     :return: None
     """
-    results = pd.read_csv(results_csv)
+    results = pd.read_csv(results_csv_path)
     plt.xscale('log')
     plt.plot(results['beta'], results['train_acc'], label='training accuracy')
     plt.plot(results['beta'], results['val_acc'], label='validation accuracy')
@@ -30,6 +30,34 @@ def plot_nb_gridsearch(results_csv: str, save_pth: str = None):
     if save_pth is not None:
         plt.savefig(save_pth, dpi=300)
     plt.show()
+
+
+def plot_lr_gridsearch(results_csv_path: str, save_pth: str = None):
+    """
+    Plot the results from LR gridsearch
+    :param results_csv_path: str, path to results csv file
+    :param save_pth: str (optional), path to save figure to
+    :return: None
+    """
+    results = pd.read_csv(results_csv_path, index_col=0)
+    keys = ["eta", "lambda", "n_iter"]
+    xlabels = {"eta": '\u03B7', "lambda": '\u03BB', "n_iter": "num. iterations"}
+    fig, axs = plt.subplots(nrows=1, ncols=len(keys), figsize=(16, 6), sharey='row')
+    axs[0].set_ylabel('Accuracy')
+    for i in range(len(keys)):
+        key = keys[i]
+        ax = axs[i]
+        results_avg = results.groupby(key).mean()
+        ax.set_xscale('log')
+        ax.plot(results_avg.index, results_avg['train_acc'], label='training accuracy')
+        ax.plot(results_avg.index, results_avg['val_acc'], label='validation accuracy')
+        ax.legend()
+        # ax.title('LR accuracy for different values of \u03B7')
+        ax.set_xlabel(xlabels[key])
+    if save_pth is not None:
+        plt.savefig(save_pth, dpi=300)
+    plt.show()
+
 
 def plot_confusion_matrix(c: np.ndarray, title: str = None, save_pth: str = None):
     """
@@ -60,9 +88,12 @@ def plot_confusion_matrix(c: np.ndarray, title: str = None, save_pth: str = None
         plt.savefig(save_pth, dpi=300)
     plt.show()
 
+
 if __name__ == "__main__":
     nb_csv = "../results/nb_results.csv"
+    lr_csv = "../results/lr_results_combined1.csv"
     import os
+
     os.makedirs("../figures", exist_ok=True)
     save_path = "../figures/nb_beta_acc.png"
-    plot_nb_gridsearch(nb_csv, save_path)
+    plot_lr_gridsearch(lr_csv, None)
